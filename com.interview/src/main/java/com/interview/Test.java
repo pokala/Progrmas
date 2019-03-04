90173bb54853a572f9d462cc035bbe01e5824fa4
Ravikanth Pokala <ravikanth.pokala@doverfs.com>
2019-03-04 12:40:00 +0530

    Updated First Class

---
+++ com.interview/src/main/java/com/interview/Test.java
@@ -0,0 +1,19 @@
+package com.interview;
+
+public class Test {
+	
+ public static int a = 1;
+ public static int b = 6;
+
+ public static void main(String args[]) {
+	 System.out.println(add(a,b));
+ }
+ 
+ 
+ public static int add(int a,int b) {
+	 int c = a+b;
+	return c;
+	 
+ }
+
+}
