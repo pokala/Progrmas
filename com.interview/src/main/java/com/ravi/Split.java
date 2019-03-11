d794b92e6ea8d12d9697e0e68da28fea6a116fb8
Ravikanth Pokala <ravikanth.pokala@doverfs.com>
2019-03-11 13:53:26 +0530

    Merged Code

--- com.interview/src/main/java/com/ravi/Split.java
+++ com.interview/src/main/java/com/ravi/Split.java
@@ -1,7 +1,7 @@
 package com.ravi;
 
 public class Split {
-    public  static String value ="text-anchor: middle; opacity: 0.287175; fill: rgb(0, 0, 0); font-size: 10px;";
+    public  static String value ="text-anchor: middle; opacity: 0.287175; fill: rgb(0, 0, 0); font-size: 10px; ";
 
 	   public static void main(String []args){
 	        
